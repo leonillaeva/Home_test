f35f175939d92b33244fb5499446299906d15d2c
yelyzaveta.herasymenko <yelyzaveta.herasymenko@awwcor.com>
2022-03-27 15:34:55 +0300

    1 index_php created

---
+++ index.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	
+	<link href="css/reset.css" rel="stylesheet" type="text/css" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	<title>Интернет-Магазин</title>
+</head>
+<body>
+<div id="block-body">
+<?php
+include ("include/block-header.php");
+?>
+<div id="block-right"></div>
+<div id="block-content"></div>
+<?php
+include ("include/block-footer.php");
+?>
+</div>
+</body>
+</html>
